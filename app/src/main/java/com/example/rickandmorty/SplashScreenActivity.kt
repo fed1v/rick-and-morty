a1e8c49e31568b48f7f53573d8788154d244b08f
fedivmi@gmail.com <fedivmi@gmail.com>
2022-06-16 19:15:31 +0500

    added splashscreen

--- app/src/main/java/com/example/rickandmorty/SplashScreenActivity.kt
+++ app/src/main/java/com/example/rickandmorty/SplashScreenActivity.kt
@@ -1,12 +1,14 @@
 package com.example.rickandmorty
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        Thread.sleep(3000L)
+        startActivity(Intent(this, MainActivity::class.java))
+        finishAffinity()
     }
 }
